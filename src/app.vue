a58ee8034c76ab14453f88e8c73d3187f915388c
liukaixin01 <1360517661@qq.com>
2018-02-27 22:14:12 +0800

    webpack 初始化工程

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="text">{{text}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'hello vue'
+            }
+        }
+    }
+</script>
+
+<style>
+    #text{
+        color: red
+    }
+</style>
