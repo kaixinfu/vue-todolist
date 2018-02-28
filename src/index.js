f728dd7c263cb359aac0137aba55d050f607fcf2
liukaixin01 <1360517661@qq.com>
2018-02-28 21:44:30 +0800

    配置打包文件以及静态资源

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import APP from './app.vue';
 
+import './assets/styles/test.css'
+import './assets/imgs/backgroud.jpg'
+import './assets/styles/test.stylus.styl'
+
 const root = document.createElement('div');
 document.body.appendChild(root);
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,29 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.vue$/,
+                test: /\.vue$/,
                 loader: 'vue-loader'
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.styl/,
+                use: ['style-loader', 'css-loader', 'stylus-loader']
+            },
+            {
+                test: /\.(jpg|gif|jpeg|svg|png)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
         ]
     }
 }
